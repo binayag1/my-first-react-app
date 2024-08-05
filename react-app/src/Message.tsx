7e32c95a8080efc99c0991caa91d3872119ce346
Binaya <binaya.ghimire@cubaneconsulting.com>
2024-08-05 12:00:55 +0930

    Creating first react component called Message.tsx

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,15 @@
+
+//PascalCasing
+function Message() {
+    //JSX : Javascript XML
+
+    const name = "Binaya";
+
+    if (name) {
+        return <h1>Hello {name}</h1>
+    } else {
+        return <h1> Hello world !!!</h1>
+    }
+}
+
+export default Message;
