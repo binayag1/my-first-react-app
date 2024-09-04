88ee5c8e6c3059937e3ee2e62711f6023a2e61a2
Binaya <binaya.ghimire@cubaneconsulting.com>
2024-09-04 17:03:16 +0930

    Using hook to set the data

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,6 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 function ListGroup() {
+
   let items = [
     "Sydney",
     "Canberra",
@@ -9,19 +10,15 @@ function ListGroup() {
     "Tasmania",
   ];
 
-  //Event handler 
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  
-  }
-  
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && "No items found"}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>{item}</li>
+          <li className={selectedIndex === index ? 'list-group-item active': 'list-group-item'} key={item} onClick={()=>{setSelectedIndex(index)}}>{item}</li>
         ))}
       </ul>
     </>
